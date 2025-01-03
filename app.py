53bea043bcb90dea16685708e6e568ee7ace71f8
nooriqbalx <140512512+nooriqbalx@users.noreply.github.com>
2025-01-03 21:39:59 +0500

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 df = pd.read_excel("Enhanced_Dummy_HBL_Data.xlsx")
 
